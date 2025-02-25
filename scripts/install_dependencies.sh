ba73b9159fd26e29f8c7c08bcb792e22653a62f5
Beliakov Vladimir <ermegilius@gmail.com>
2025-02-25 14:26:45 +0200

    install node

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,3 +6,12 @@ wget https://aws-codedeploy-eu-north-1.s3.eu-north-1.amazonaws.com/latest/instal
 chmod +x ./install
 sudo ./install auto
 sudo pip install awscli
+
+# Install nvm (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+# Install Node.js using nvm
+nvm install node
+nvm use node
