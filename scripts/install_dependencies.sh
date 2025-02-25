cecad34faf19da2af65841c1c3cd7e81c0332f91
Beliakov Vladimir <ermegilius@gmail.com>
2025-02-25 14:14:11 +0200

    fix to north

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,7 +2,7 @@
 sudo yum -y update
 sudo yum -y install ruby wget python-pip
 cd /home/ec2-user
-wget https://aws-codedeploy-eu-central-1.s3.eu-central-1.amazonaws.com/latest/install
+wget https://aws-codedeploy-eu-north-1.s3.eu-north-1.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
 sudo pip install awscli
